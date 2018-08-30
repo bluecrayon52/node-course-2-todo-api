2302a2a42f102924f829eb298d9f904dbc3c1637
Nathan Arnold <16687465+bluecrayon52@users.noreply.github.com>
2018-08-29 20:51:43 -0400

    added POST /todos route to server

--- server/server.js
+++ server/server.js
@@ -32,6 +32,14 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});  // send back object en lieu of array 
+    },(e) => {
+        res.status(400).send(e); 
+    });
+});
+
 app.listen(3000, () => {
     console.log('Start on port 3000'); 
 }); 
