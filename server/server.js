d2339cd0d1036bceaa4c240a6485c1c20a9b037a
Nathan Arnold <16687465+bluecrayon52@users.noreply.github.com>
2018-09-05 15:37:42 -0400

    added PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
-var express = require('express'); 
-var bodyParser = require('body-parser');
+const _ = require('lodash'); 
+const express = require('express'); 
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb'); 
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
 var app = express(); 
+
 const port = process.env.PORT || 3000; 
+
 app.use(bodyParser.json()); 
 
 //---------------------------------POST--------------------------------------------------------------
@@ -74,6 +79,34 @@ app.delete('/todos/:id', (req, res) => {
         }).catch((e) => { 
             res.status(400).send();
         });
+});
+
+//---------------------------------PATCH--------------------------------------------------------------
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id; 
+    var body = _.pick(req.body, ['text', 'completed']); // limit what the user can edit 
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send('Todo Id not valid');  
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime(); 
+
+    } else {
+        body.completed = false; 
+        body.completedAt =null; 
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send(); 
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send(e); 
+    })
+
 });
 
 app.listen(port, () => {
